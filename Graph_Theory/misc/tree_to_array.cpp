8a1e9dcce2e9755e9b4f66c3a479a03169533b62
Nithin <kannasasuke19@gmail.com>
2017-11-05 10:38:27 +0530

    updated variable names to avoid ambiguity

--- Graph_Theory/misc/tree_to_array.cpp
+++ Graph_Theory/misc/tree_to_array.cpp
@@ -2,12 +2,12 @@ const int N; // define n as number of nodes, so size of array can be 2*n;
 
 vector<int> adj[N];
 
-int begin[2*N];       // denotes begining of v's subtree
-int end[2*n];        // denotes ending index of v's subtree
-int discover[2*n];  // array on which you have to built segment tree
-int pos[2*n];      // position of a node in discover array
+int BEGIN[2*N];       // denotes begining of v's subtree
+int END[2*N];        // denotes ending index of v's subtree
+int discover[2*N];  // array on which you have to built segment tree
+int position[2*N];      // position of a node in discover array
 
-vector<bool> vis(n,0);
+vector<bool> vis(N,0);
 
 int cnt = -1;    // for 0-indexed indexing of arrays
 
@@ -15,10 +15,10 @@ void dfs(int u)
 {
 	vis[u] = 1;
 
-	discover[++cnt]=u; // discovering that node for first time
-	pos[u]=cnt;       // storing position of that node in discover array
+	discover[++cnt] = u; // discovering that node for first time
+	position[u] = cnt;       // storing position of that node in discover array
 	
-	begin[u]=cnt;    // storing begining index of that node in discover array
+	BEGIN[u] = cnt;    // storing begining index of that node in discover array
 	
 	for(int i = 0; i < adj[u].size(); i++)
 	{
@@ -26,5 +26,5 @@ void dfs(int u)
 			dfs(adj[u][i]);
 	}
 
-	end[u]=cnt;  // storing ending index of that node in discover array 
+	END[u] = cnt;  // storing ending index of that node in discover array 
 }
